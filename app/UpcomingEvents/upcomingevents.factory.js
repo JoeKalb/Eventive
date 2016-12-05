6af2498c8e1306520393f7841eaeb6255794672f
Joseph Kalb <Joseph Kalb>
2016-12-05 14:54:38 -0800

    more connections made

--- app/UpcomingEvents/upcomingevents.factory.js
+++ app/UpcomingEvents/upcomingevents.factory.js
@@ -7,7 +7,9 @@
     /* @ngInject */
     function EventsFactory($http, $q, wineServer) {
         var service = {
-            getAllEvents: getAllEvents
+            getAllEvents: getAllEvents, 
+            addToEvent: addToEvent,
+            getEventsByProfile: getEventsByProfile
         };
         return service;
         ////////////////
@@ -32,6 +34,61 @@
         	});
 
         	return defer.promise;
+        }
+
+        function addToEvent(eventId, userId, userName, userNumber, token) {
+            var defer = $q.defer();
+            var bodyData = {
+                "attendeeid": userId, 
+                "attendeename": userName, 
+                "attendeenumber": userNumber,
+                "checkin": false
+            }
+            console.log(bodyData);
+            $http({
+                method: 'PUT',
+                url: wineServer + 'events/' + eventId,
+                data: bodyData,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
+            }).then(function(response){
+                if (typeof response.data === 'object'){
+                    defer.resolve(response.data);
+                } else {
+                    defer.reject(response);
+                }
+            }, 
+            function(error) {
+                defer.reject(error);
+            });
+
+            return defer.promise;
+        }
+
+        function getEventsByProfile(profileId, token) {
+            var defer = $q.defer();
+
+            $http({
+                method: 'GET',
+                url: wineServer + 'events/profile/' + profileId,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
+            }).then(function(response) {
+                if (typeof response.data === 'object'){
+                    defer.resolve(response.data);
+                } else {
+                    defer.reject(response);
+                }
+            },
+            function(error) {
+                defer.reject(error);
+            });
+
+            return defer.promise;
         }
     }
 })();
