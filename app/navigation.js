cff7e0a64aba4ed60ed3cfb99bd5f473415cc1bd
john abanto <jabanto85@gmail.com>
2016-12-14 14:20:36 -0800

    testing new plugin

--- app/navigation.js
+++ app/navigation.js
@@ -30,3 +30,7 @@ function showSlides(n) {
 	slides[slideIndex-1].style.display = "block"; 
 	dots[slideIndex-1].className += " active";
 };
+
+
+// animate css 
+$('#yourElement').animateCss('fadeInLeft');
