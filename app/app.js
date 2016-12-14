0dfc10d98f51a4030d5d20f296dfb68a8404b5a8
Joseph Kalb <Joseph Kalb>
2016-12-13 19:10:58 -0800

    more things

--- app/app.js
+++ app/app.js
@@ -10,8 +10,8 @@
             'ngFileUpload'
         ]);
 
-        app.value ('wineServer', 'https://appventful.herokuapp.com/api/');
-        //app.value ('wineServer', 'http://localhost:3000/api/');
+        //app.value ('wineServer', 'https://appventful.herokuapp.com/api/');
+        app.value ('wineServer', 'http://localhost:3000/api/');
         app.config(['$stateProvider', '$urlRouterProvider', 'localStorageServiceProvider',  function($stateProvider, $urlRouterProvider, localStorageServiceProvider) {
         	$urlRouterProvider.otherwise('/home');
 
